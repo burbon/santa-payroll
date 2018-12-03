a1bbfb5ac96a991db00b3feaa8e5a9f60c4418e5
Błażej Cegiełka <blazej.cegielka@gmail.com>
2018-12-03 21:50:11 +0000

    Implemented payroll formula
    Added Elf class with age method

--- payroll.py
+++ payroll.py
@@ -1,2 +1,14 @@
-def payroll():
-    return 1
+class Elf(object):
+    def __init__(self, birthday):
+        self.birthday = birthday
+
+    def age(self, date):
+        diff = date.year - self.birthday.year
+        if (date.month, date.day) < (self.birthday.month, self.birthday.day):
+            diff -= 1
+
+        return diff
+
+
+def payroll(elf, payday):
+    return round((elf.age(payday) * 52) / 12)
--- test_payroll.py
+++ test_payroll.py
@@ -1,5 +1,36 @@
-from payroll import payroll
+from datetime import date
+
+from payroll import payroll, Elf
 
 
-def test_payroll():
-    assert payroll() == 1
+def test_elf_age_had_birthday_that_year():
+    elf = Elf(date.fromisoformat('2007-01-02'))
+    on_date = date.fromisoformat('2019-01-02')
+    assert elf.age(on_date) == 12
+
+
+def test_elf_age_no_birthday_that_year_yet():
+    elf = Elf(date.fromisoformat('2007-01-02'))
+    on_date = date.fromisoformat('2019-01-01')
+    assert elf.age(on_date) == 11
+
+
+def test_payroll_integer():
+    elf = Elf(date.fromisoformat('2007-01-01'))
+    payday = date.fromisoformat('2019-01-01')
+    # (12 * 52) / 12 = 52
+    assert payroll(elf, payday) == 52
+
+
+def test_payroll_round_down():
+    elf = Elf(date.fromisoformat('2007-01-01'))
+    payday = date.fromisoformat('2008-01-01')
+    # (1 * 52) / 12 = 4.33
+    assert payroll(elf, payday) == 4
+
+
+def test_payroll_round_up():
+    elf = Elf(date.fromisoformat('2007-01-01'))
+    payday = date.fromisoformat('2009-01-01')
+    # (2 * 52) / 12 = 8.66
+    assert payroll(elf, payday) == 9
