372b4cc595a1c90bc4fef0dedeee87af710a7bb9
Błażej Cegiełka <blazej.cegielka@gmail.com>
2018-12-04 22:18:52 +0000

    Added JarSplit
    handles charity, retirement and candy splits

--- payroll.py
+++ payroll.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+
 class Elf(object):
     def __init__(self, birthday):
         self.birthday = birthday
@@ -8,6 +11,15 @@ class Elf(object):
             diff -= 1
 
         return diff
+
+
+class JarSplit(object):
+    def __init__(self, amount):
+        self.amount = amount
+
+        self.charity = Decimal('0.1') * self.amount
+        self.retirement = Decimal('0.4') * self.amount
+        self.candy = Decimal('0.5') * self.amount
 
 
 def payroll(elf, payday):
--- test_payroll.py
+++ test_payroll.py
@@ -1,6 +1,7 @@
 from datetime import date
+from decimal import Decimal
 
-from payroll import payroll, Elf
+from payroll import payroll, Elf, JarSplit
 
 
 def test_elf_age_had_birthday_that_year():
@@ -15,22 +16,36 @@ def test_elf_age_no_birthday_that_year_yet():
     assert elf.age(on_date) == 11
 
 
-def test_payroll_integer():
+def test_payroll_whole():
     elf = Elf(date.fromisoformat('2007-01-01'))
     payday = date.fromisoformat('2019-01-01')
     # (12 * 52) / 12 = 52
     assert payroll(elf, payday) == 52
 
 
-def test_payroll_round_down():
+def test_payroll_fraction_round_down():
     elf = Elf(date.fromisoformat('2007-01-01'))
     payday = date.fromisoformat('2008-01-01')
     # (1 * 52) / 12 = 4.33
     assert payroll(elf, payday) == 4
 
 
-def test_payroll_round_up():
+def test_payroll_fraction_round_up():
     elf = Elf(date.fromisoformat('2007-01-01'))
     payday = date.fromisoformat('2009-01-01')
     # (2 * 52) / 12 = 8.66
     assert payroll(elf, payday) == 9
+
+
+def test_jar_split_whole():
+    jar_split = JarSplit(100)
+    assert jar_split.charity == 10
+    assert jar_split.retirement == 40
+    assert jar_split.candy == 50
+
+
+def test_jar_split_fraction():
+    jar_split = JarSplit(3)
+    assert jar_split.charity == Decimal('0.3')
+    assert jar_split.retirement == Decimal('1.2')
+    assert jar_split.candy == Decimal('1.5')
